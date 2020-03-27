5364c1790f8b3ee68bafee41715cc83eb5fc3387
onuoha collins <onuoha.collins@gmail.com>
2020-03-27 20:45:55 +0100

    added new files

---
+++ index.js
@@ -0,0 +1,10 @@
+let name ="Onuoha Collins";
+let courses=["html","css","javascript","ux/ui"]
+console.log("My name is:" + name +"these are my courses"=courses);
+const data =[];
+for (let i=0; i<=200; i++){
+    if (i%2===0){
+    dat.push(i);
+    }
+}
+console.log(data);
